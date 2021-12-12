1d1f1c54d2036ce3e53a4d8c26b504d4cea5c8b3
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-12-12 14:25:19 +0700

    Update 2021_12_12_053824_create_hasilnilais_table.php

--- database/migrations/2021_12_12_053824_create_hasilnilais_table.php
+++ database/migrations/2021_12_12_053824_create_hasilnilais_table.php
@@ -31,6 +31,7 @@ class CreateHasilnilaisTable extends Migration
      */
     public function down()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('hasilnilais');
     }
 }
