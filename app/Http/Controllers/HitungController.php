d69b0a1a71974f5ec592d3d2cf339d9ed90e2168
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-12-14 01:46:48 +0700

    Update HitungController.php

--- app/Http/Controllers/HitungController.php
+++ app/Http/Controllers/HitungController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Alternatif;
+use App\Models\Kriteria;
 
 class HitungController extends Controller
 {
@@ -19,13 +20,41 @@ class HitungController extends Controller
 
     public function store(Request $request)
     {
-        $data = $request->all();
-        $hasil_alternatif = new Alternatif;
-
-        foreach ($data as $key => $value) {
-           
-            return $value;
+        $matriks=[];
+        $buffer= 0;
+        $total= [];
+        $i=0;
+        $bobot= Kriteria::select('bobot')->get()->toArray();
+        
+        foreach ($request->bentuk_batang as $key => $val) {
+            $matriks[$key][0] = round(($val/max($request->bentuk_batang)),2); 
         }
+        foreach ($request->warna_batang as $key => $val) {
+            $matriks[$key][1] = round(($val/max($request->warna_batang)),2); 
+        }
+        foreach ($request->daun as $key => $val) {
+            $matriks[$key][2] = round(($val/max($request->daun)),2); 
+        }
+        foreach ($request->pucuk_daun as $key => $val) {
+            $matriks[$key][3] = round(($val/max($request->pucuk_daun)),2); 
+        }
+    
+        foreach($matriks as $matrik){
+            foreach($matrik as $key => $val){
+                $buffer= round(($buffer + ($val*$bobot[$key]['bobot'])),3);
+            }
+            $total[$i++] = $buffer;
+            $buffer=0;
+        }
+        $total= collect($total)->sort()->reverse();
+        $r=0;
+        foreach ($total as $key => $val ){
+            $hasil = new Alternatif;
+            $hasil->nama_bibit = $request->nama_bibit;
+            $hasil->nilai = $val;
+            $hasil->rangking = $r++;
+        }
+
     }
 
     public function show($id)
