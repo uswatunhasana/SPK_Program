d55fb66a9e320d80d488e4912a59a57f900f9b55
uswatunhasana <57187364+uswatunhasana@users.noreply.github.com>
2021-12-14 07:52:29 +0700

    Update HitungController.php

--- app/Http/Controllers/HitungController.php
+++ app/Http/Controllers/HitungController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Alternatif;
+use RealRashid\SweetAlert\Facades\Alert;
+use App\Models\Kategori;
+
 use App\Models\Kriteria;
 
 class HitungController extends Controller
 {
     public function index()
     {
-        //
+        $batangs = Kategori::where('kriteria_id','1')->select('*')->get();
+        $warnas = Kategori::where('kriteria_id','2')->select('*')->get();
+        $dauns = Kategori::where('kriteria_id','3')->select('*')->get();
+        $pucuks = Kategori::where('kriteria_id','4')->select('*')->get();
+        $hasils = Alternatif::with(['alternatif'])->orderBy('rangking', 'ASC')->get();
+        $jumlah_alternatif = Alternatif::count();
+        return view('welcome',compact('batangs','warnas','pucuks','dauns','hasils','jumlah_alternatif'));
     }
 
     public function create()
@@ -20,41 +29,44 @@ class HitungController extends Controller
 
     public function store(Request $request)
     {
-        $matriks=[];
-        $buffer= 0;
-        $total= [];
-        $i=0;
-        $bobot= Kriteria::select('bobot')->get()->toArray();
-        
+        Alternatif::query()->truncate();
+        $matriks = [];
+        $buffer = 0;
+        $total = [];
+        $i = 0;
+        $bobot = Kriteria::select('bobot')->get()->toArray();
+
         foreach ($request->bentuk_batang as $key => $val) {
-            $matriks[$key][0] = round(($val/max($request->bentuk_batang)),2); 
+            $matriks[$key][0] = round(($val / max($request->bentuk_batang)), 2);
         }
         foreach ($request->warna_batang as $key => $val) {
-            $matriks[$key][1] = round(($val/max($request->warna_batang)),2); 
+            $matriks[$key][1] = round(($val / max($request->warna_batang)), 2);
         }
         foreach ($request->daun as $key => $val) {
-            $matriks[$key][2] = round(($val/max($request->daun)),2); 
+            $matriks[$key][2] = round(($val / max($request->daun)), 2);
         }
         foreach ($request->pucuk_daun as $key => $val) {
-            $matriks[$key][3] = round(($val/max($request->pucuk_daun)),2); 
-        }
-    
-        foreach($matriks as $matrik){
-            foreach($matrik as $key => $val){
-                $buffer= round(($buffer + ($val*$bobot[$key]['bobot'])),3);
-            }
-            $total[$i++] = $buffer;
-            $buffer=0;
-        }
-        $total= collect($total)->sort()->reverse();
-        $r=0;
-        foreach ($total as $key => $val ){
-            $hasil = new Alternatif;
-            $hasil->nama_bibit = $request->nama_bibit;
-            $hasil->nilai = $val;
-            $hasil->rangking = $r++;
+            $matriks[$key][3] = round(($val / max($request->pucuk_daun)), 2);
         }
 
+        foreach ($matriks as $matrik) {
+            foreach ($matrik as $key => $val) {
+                $buffer = round(($buffer + ($val * $bobot[$key]['bobot'])), 3);
+            }
+            $total[$i++] = $buffer;
+            $buffer = 0;
+        }
+        $total = collect($total)->sort()->reverse();
+        $i=1;
+        foreach($total as $t){
+            $hasil = new Alternatif;
+            $hasil->nama_bibit = $request->nama_bibit[$i-1];
+            $hasil->nilai= $t ;
+            $hasil->rangking = $i;
+            $hasil->save();
+            $i++;
+        }     
+        return redirect()->back();
     }
 
     public function show($id)
@@ -68,7 +80,7 @@ class HitungController extends Controller
         //
     }
 
- 
+
     public function update(Request $request, $id)
     {
         //
